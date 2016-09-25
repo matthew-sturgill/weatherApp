a29423f4520b2636dbe924245b1d724cb8d8dafd
matthew-sturgill <matthew.sturgill@interapthq.com>
2016-09-25 09:13:20 -0400

    sunday commit before trying some html changes

--- weatherApp.js
+++ weatherApp.js
@@ -1,68 +1,54 @@
 $(function () {
-    //Below works for pulling data from dark sky as well as assigning id to pull data to html
-    //$.ajax("https://api.darksky.net/forecast/71ce746e051ee5b5e05ffb182555ea67" + "/37.8267,-122.4233", { dataType: "jsonp"}).done(function(data) {
-    //   console.log(data);
-
-
-    // $("#summary").html(data.currently.summary);
-    //$("#temperature").html("Temp: " + data.currently.apparentTemperature);
-
-
-
-    // });
-
-    //Below calls zip from input to variable "area".
     $("#sendZip").click(function () {
         var area = $("#zipCode").val();
-        console.log(area);
 
 
-        //below adds variable "area" to google and retrieves lat and long
+
+        //   console.log(area);
+        //    below adds variable "area" to google and retrieves lat and long
         $.ajax("https://maps.googleapis.com/maps/api/geocode/json?address=" + area + "&key=AIzaSyAt3yHpgvBYw2u5lrWXKisJ9d23ITL3VUQ", { dataType: "json" }).done(function (data) {
             console.log(data);
             var latI = data.results[0].geometry.location.lat;
             var longI = data.results[0].geometry.location.lng;
-            console.log(latI);
-            console.log(longI);
-            
+            var town = data.results[0].formatted_address;
+            //   console.log(latI);
+            //   console.log(longI);
+            //   console.log(town);
             $.ajax("https://api.darksky.net/forecast/71ce746e051ee5b5e05ffb182555ea67/" + latI + "," + longI, { dataType: "jsonp" }).done(function (data) {
-              
-            $("#dailySummary").html(data.currently.summary);
-            $("#temperature").html(data.currently.temperature);
-            $("#temperatureMax").html(data.daily.data[0].apparentTemperatureMax);
-            $("#temperatureMin").html(data.daily.data[0].apparentTemperatureMin);
-            $("#rainPercent").html(data.daily.data[0].precipProbability);
-                //above works to pull information might turn to variable and plug into a long set up screen
+                console.log(data);
+                var dailySummary = data.currently.summary;
+                var currentTemperature = data.currently.temperature;
+                var tempHigh = data.daily.data[0].apparentTemperatureMax;
+                var tempMin = data.daily.data[0].apparentTemperatureMin;
+                var chanceOfRain = data.daily.data[0].precipProbability;
+                var displayTemperature = Math.round(currentTemperature);
+                var displayTemperatureHigh = Math.round(tempHigh);
+                var displayTemperatureMin = Math.round(tempMin);
+                console.log(dailySummary);
+                console.log(displayTemperature);
+                console.log(displayTemperatureHigh);
+                console.log(displayTemperatureMin);
+                console.log(chanceOfRain);
+                //above works to pull information turn to variable and plug into a long set up screen
+                $("#sendZip").click(function () {
+                    addContent(data);
+                })
+
+                var addContent = function (id) {
+                    var container = $("#container");
+                    var span = $("<span></span>");
+               
+
+                    container.append('<span>Hello, World!";
+                };
 
 
             });
 
-
         });
-
-  // $("#sendZip").click(function() {
-    // var addContent = function() {
-  //      var container = $("#container");
-//        var span = $("<span></span>");
-      //  span.html("temper ");
-    //    span.append("World!");
-  //      var link = $("<a></a>");
-//        link.attr("href", url);
-      //  link.attr("target", "_blank");
-    //    link.html(" (Google That) ");
-  //      span.append(link);
- //       container.append(span);
-//
-      //  container.append('<span> "Temperature);
-  
     });
 
-
-
-
 });
-
-
 
 
 
